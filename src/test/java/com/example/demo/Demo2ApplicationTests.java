453367c9af62b0e252c24b791271e47c7b05c5c1
StefanBilleb <StefanBilleb@gmx.de>
2022-07-29 11:50:00 +0200

    Initial commit

---
+++ src/test/java/com/example/demo/Demo2ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+public class Demo2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		System.out.println("Test successful");
+	}
+
+}
